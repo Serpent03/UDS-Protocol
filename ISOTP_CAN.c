1154c41608eb73c00e813a6a6e23a6439db97246
Serpent03 <dumbpilot03@gmail.com>
2024-01-28 15:32:04 +0530

    feat: single ISOTP message generation

---
+++ ISOTP_CAN.c
@@ -0,0 +1,50 @@
+#include "common.h"
+#include "ISOTP_CAN.h"
+#include "queue.h"
+
+void generate_ISOTP_frames(UDS_Packet *udsp) {
+  // put data inside a queue.
+  // reference queue to this helper function
+  queue *data_queue = init_queue(udsp->dataLength + 1);
+  enque(data_queue, udsp->SID);
+  for (uInt16 i = 0; i < udsp->dataLength; i++) {
+    enque(data_queue, udsp->data[i]);
+  }
+  uInt16 dataLength = len_queue(data_queue);
+  if (dataLength > 7) {
+    // generate_multiple_frame(queue)
+    // -> generate(first_frame)
+    // -> wait for (control flow)
+    // -> send rest of the frames
+  } else {
+    // generate_single_frame
+    // append each frame inside the ISO_TP_Frame with addr
+    CAN_Frame* cfr = single_CANTP_frame(data_queue, dataLength);
+    printf("PCI: 0x%02x\n", cfr->PCI[0]);
+    for (uInt16 i = 0; i < 7; i++) {
+      printf("DAT: 0x%02x\n", cfr->data[i]);
+    }
+    // TODO envelope the returned CAN_frame inside the ISOTP frame
+    // send_ISOTP_frames(cfr);
+  }
+  free_queue(data_queue);
+}
+
+CAN_Frame* single_CANTP_frame(queue* data_queue, uInt16 dataLength) {
+  CAN_Frame *cfr = (CAN_Frame*)calloc(1, sizeof(CAN_Frame));
+  cfr->PCI = (uInt8*)calloc(1, sizeof(uInt8));
+  cfr->PCI[0] = (CAN_CODE_CONSEC_FRAME << 4) | (dataLength & 0xF);
+
+  cfr->data = (uInt8*)calloc(7, sizeof(uInt8));
+  uInt8 idx = 0;
+  uInt8 data;
+  while (idx < 7) {
+    // at_queue_front() returns false if the queue is empty.
+    // we load the data into a memory address to get the actual data.
+    bool opSuccess = at_queue_front(data_queue, &data);
+    cfr->data[idx] = opSuccess ? data : ISOTP_PADDING;
+    dequeue(data_queue);
+    idx++;
+  }
+  return cfr;
+}
---
+++ ISOTP_CAN.h
@@ -0,0 +1,42 @@
+#include "common.h"
+#include "UDS.h"
+#include "queue.h"
+
+#define ISOTP_PADDING 0xAA
+
+enum CAN_FRAME_CODES {
+  CAN_CODE_SINGLE_FRAME = 0x0,
+  CAN_CODE_FIRST_FRAME = 0x1,
+  CAN_CODE_CONSEC_FRAME = 0x2,
+  CAN_CODE_FLOW_CNTL_FRAME = 0x3 
+};
+
+enum CAN_FRAME_TYPE {
+  CAN_TYPE_FLOW_CONTROL,
+  CAN_TYPE_SINGLE_FRAME,
+  CAN_TYPE_FIRST_FRAME,
+  CAN_TYPE_CONSEC_FRAME
+};
+
+typedef struct CAN_Frame {
+  // this can only be 8 bytes long everytime. Either segment or pad!
+  uInt8 *PCI;
+  uInt8 *data;
+} CAN_Frame;
+
+typedef struct ISO_TP_Frame {
+  uInt16 addr;
+  CAN_Frame *cfr; 
+} ISO_TP_Frame;
+
+enum defaultAddress {
+  DEFAULT_RX_ADDR = 0x6789,
+  DEFAULT_TX_ADDR = 0x1234,
+};
+
+CAN_Frame* single_CANTP_frame(queue* data_queue, uInt16 dataLength);
+CAN_Frame* multiple_CANTP_frame(queue* data_queue);
+
+void generate_ISOTP_frames(UDS_Packet *udsp);
+void send_ISOTP_frames(CAN_Frame* cfr);
+
