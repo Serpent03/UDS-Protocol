4687f57e0704ad11b05f9db8058ea321a3a2de90
Serpent03 <dumbpilot03@gmail.com>
2024-02-13 21:13:46 +0530

    feat: CAN BUS simulation

--- BUS/bus.c
+++ BUS/bus.c
@@ -1,5 +1,50 @@
 #include "bus.h"
+#include "../SERVICER/servicer.h"
 
+FILE *bus;
+uInt8 NUL_BUF[10] = { 0 };
+uInt8 BUS_DATA[10];
+SESSION can_session;
 
+void set_session(uInt16 start, uInt16 end) {
+  can_session.RANGE_START = start;
+  can_session.RANGE_END = end;
+}
 
+void check_bus() {
+  /** 
+   * @todo Implement trigger based file reading. 
+   * @todo Add in address filtration.
+   */
+  bus = fopen("bus.bin", "rb");
+  if (bus == NULL) {
+    return;
+  }
+  fread(BUS_DATA, 10, sizeof(uInt8), bus);
+  if (memcmp(BUS_DATA, NUL_BUF, 10) == 0) {
+    return;
+  }
+  uInt16 CAN_ID = ((BUS_DATA[0] << 8) | BUS_DATA[1]) >> 5;
+  if (CAN_ID < can_session.RANGE_START || CAN_ID > can_session.RANGE_END) {
+    return;
+  }
+  receiveFlag = true;
+  /* At this point, the servicer() takes over. */
+}
+
+void write_to_bus(uInt8 *OUT_BUF, size_t size) {
+  bus = fopen("bus.bin", "ab");
+  fwrite(OUT_BUF, sizeof(uInt8), size, bus);
+  fclose(bus);
+}
+
+bool read_from_bus(uInt8 *IN_BUF, size_t size) {
+  bus = fopen("bus.bin", "rb");
+  if (bus == NULL) {
+    return false;
+  }
+  fread(IN_BUF, size, sizeof(uInt8), bus);
+  fclose(bus);
+  return true;
+}
 
--- BUS/bus.h
+++ BUS/bus.h
@@ -10,10 +10,42 @@
  */
 
 typedef struct SESSION {
-  uInt16 CLIENT_ADDR;
-  uInt16 SERVER_ADDR;
+  uInt16 RANGE_START;
+  uInt16 RANGE_END;
 } SESSION;
+extern SESSION can_session;
 
-/** @todo I/O triggers can be simulated here. */
+/**
+ * @brief Set the CAN-ID session range.
+ * @param start Start of the 11-bit range.
+ * @param end End of the 11-bit range.
+ * @returns void.
+ * @todo remove dependency on FILE I/O.
+ */
+void set_session(uInt16 start, uInt16 end);
 
+/**
+ * @brief Monitor the bus for any activity.
+ * @returns void.
+ * @todo remove dependency on FILE I/O.
+ */
+void check_bus();
+
+/**
+ * @brief Write data to the CAN bus.
+ * @param OUT_BUF The output buffer to write to the bus.
+ * @param size Size of the output buffer.
+ * @returns void.
+ * @todo remove dependency on FILE I/O.
+ */
+void write_to_bus(uInt8 *OUT_BUF, size_t size);
+
+/**
+ * @brief Read data from the CAN bus.
+ * @param IN_BUF The input buffer to populate.
+ * @param size Size of the input buffer.
+ * @returns True if operation was successful.
+ * @todo remove dependency on FILE I/O.
+ */
+bool read_from_bus(uInt8 *IN_BUF, size_t size);
 
