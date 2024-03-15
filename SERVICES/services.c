63769afa2854cb2866f00078ff4bcca09086b336
Serpent03 <dumbpilot03@gmail.com>
2024-03-15 12:21:34 +0530

    feat: #10 inclusion of a security/param guard for all SID wrappers

--- SERVICES/services.c
+++ SERVICES/services.c
@@ -2,6 +2,7 @@
 #include "../SESSION/state.h"
 
 uInt8 resp_data[4096];
+service_functions service_table[NUM_SERVICES];
 
 void set_failure(UDS_Packet *rx, uInt8 *resp_data, uInt16 *idx, enum NEG_RESPONSE_CODES fail_code) {
   // limit the length to two automatically for sending failure data.
@@ -14,6 +15,29 @@ void set_failure(UDS_Packet *rx, uInt8 *resp_data, uInt16 *idx, enum NEG_RESPONS
  * @todo switch to a struct based function caller instead of switch. 
  * @todo add in security level restrictions before entering a function.
  */
+
+
+void init_service_table() {
+  service_table[0].SID = SID_DIAGNOSTIC_SESS_CNTL;
+  service_table[0].callback_function = handle_diag_sess_cntl;
+  service_table[0].security_level = 0x0;
+  service_table[0].diag_sess = 0x1;
+
+  service_table[1].SID = SID_ECU_RESET;
+  service_table[1].callback_function = handle_ecu_reset;
+  service_table[1].security_level = 0x0;
+  service_table[1].diag_sess = 0x1;
+
+  service_table[2].SID = SID_TESTER_PRESENT;
+  service_table[2].callback_function = handle_tester_present;
+  service_table[2].security_level = 0x0;
+  service_table[2].diag_sess = 0x1;
+
+  service_table[3].SID = SID_SECURITY_ACCESS_SERVICE;
+  service_table[3].callback_function = handle_security_access;
+  service_table[3].security_level = 0x0;
+  service_table[3].diag_sess = 0x2 | 0x3; /* programming/extended diagnostic session */
+}
 
 UDS_Packet* service_handler(UDS_Packet *rx, bool *silenceTx) {
   UDS_Packet *tx; 
--- SERVICES/services.h
+++ SERVICES/services.h
@@ -3,7 +3,28 @@
 #include "../common.h"
 #include "../UDS/UDS.h"
 
+#define NUM_SERVICES 5
+
+/* ====== STRUCTURES ====== */
+  
+typedef struct service_functions {
+  uInt8 SID;
+  bool (*callback_function)(UDS_Packet *rx, uInt8 *resp_data, uInt16 *idx);
+  uInt8 diag_sess;
+  uInt8 security_level;
+} service_functions;
+
+extern service_functions service_table[NUM_SERVICES];
+
 /* ====== UTILITY ====== */
+
+/**
+ * @brief Set up the service table for referencing the SID wrapper functions.
+ * The service table needs to be set up with all implemented UDS routines otherwise
+ * they will be unreachable from the event loop.
+ * @return void.
+ */
+void init_service_table();
 
 /**
  * @brief Sets the outgoing UDS packet SID and data to report a failure.
